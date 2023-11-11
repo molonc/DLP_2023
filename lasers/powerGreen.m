943c4fd5dd6824087163e263a1a107e7407926a2
sayeholoumi <53805786+sayeholoumi@users.noreply.github.com>
2023-11-10 16:14:52 -0800

    Delete lasers directory

--- lasers/powerGreen.m
+++
@@ -1,6 +0,0 @@
-function [hObject, handles] = powerGreen(hObject, handles)
-    greenPowerDec = get(BLUEEditField, 'String');
-    greenPowerHex = dec2hex(255-greenPowerDec, 2);
-    powerGreenCmd = sscanf(['53 18 03 04 F', greenPowerHex, '0 50'], '%2X');
-    fwrite(app.laser, powerGreenCmd, 'uint8');
-end
