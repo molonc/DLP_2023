1435812ca9d792c6ce69e3dde63a4d8012de7ff0
benrob13 <144270914+benrob13@users.noreply.github.com>
2023-11-10 16:34:08 -0800

    Add files via upload

--- acquisition/aquisition.m
+++ acquisition/aquisition.m
@@ -13,12 +13,26 @@ for i = 1:rows
     % For odd rows, move left to right
     if mod(i, 2) ~= 0
         for j = 1:cols
-            
+            calcdisplacement(app, i, j);
+            %pause(waitBeforeMoving(app)) need to figure out how long to
+            %wait 
+
+            %turn the correct laser on 
+
+            %imaging 
+            imaging(app,i,j); 
         end
     else
         % For even rows, move right to left
         for j = cols:-1:1
-            
+            calcdisplacement(app, i, j);
+            %pause(waitBeforeMoving(app)) need to figure out how long to
+            %wait 
+
+            %turn the correct laser on 
+
+            %imaging 
+            imaging(app,i,j); 
         end
     end
 end
---
+++ acquisition/imaging.m
@@ -0,0 +1,53 @@
+%this function images splits the images and saves them 
+function imaging(app, row, col)
+
+    inputImage = snapshot(app.vidobj); % this is how you take a image using a GigE cam which point grey is ??? 
+   
+    % Get the size of the input image
+    [rows, cols] = size(inputImage);
+
+    % Ensure the image can be evenly split into a 3x2 grid
+    if mod(rows, 3) ~= 0 || mod(cols, 2) ~= 0
+        error('Image size is not suitable for splitting into a 3x2 grid.');
+    end
+
+    % Calculate the size of each portion
+    portionRows = rows / 3;
+    portionCols = cols / 2;
+
+    % Initialize the output cell array to store the portions
+    splitImage = cell(3, 2);
+
+    % Split the image into 3x2 portions
+    for i = 1:3
+        for j = 1:2
+            % Calculate the indices for each portion
+            startRow = (i - 1) * portionRows + 1;
+            endRow = i * portionRows;
+            startCol = (j - 1) * portionCols + 1;
+            endCol = j * portionCols;
+
+            % Extract the portion from the input image
+            splitImage{i, j} = inputImage(startRow:endRow, startCol:endCol);
+        end
+    end
+    
+
+    %now the image is split inside of the matrix splitimage[3][2]
+
+    for i = 1:3
+        for j = 1:2
+            % Build the file name based on row, column, i, and j
+            fileName = sprintf('Image_Row%d_Col%d_Part%d_%d.tif', row, col, i, j); %probably need to add the laser that is being imaged as well 
+
+            
+            % Get the full file path
+            fullFileName = fullfile(app.dname, fileName);
+            
+            % Save the split image
+            imwrite(splitImage{i, j}, fullFileName);
+        end
+    end
+
+
+end 
