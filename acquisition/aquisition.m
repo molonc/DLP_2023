22037cfb04258271043bdd0a146a624660d13378
sayeholoumi <53805786+sayeholoumi@users.noreply.github.com>
2023-11-14 15:16:05 -0800

    Add files via upload

---
+++ acquisition/aquisition.m
@@ -0,0 +1,56 @@
+% this is aquisition.m 
+function aquisition (app)
+   % Assuming you have a 24x36 matrix named 'inputMatrix'
+rows = 24;
+cols = 36;
+
+
+
+%this is just the loop format for moving along the matrix in a snaking
+%pattern
+
+for i = 1:rows
+    % For odd rows, move left to right
+    if mod(i, 2) ~= 0
+        for j = 1:cols
+            calcdisplacement(app, i, j);
+            %pause(waitBeforeMoving(app)) need to figure out how long to
+            %wait 
+
+            %turn the correct laser on 
+            if app.checkBlue == 1
+                turnonLaser(440)
+            elseif app.checkGreen == 1
+                turnonLaser(550)
+            elseif app.checkCyan == 1
+                turnonLaser(470)
+            end
+
+            %imaging 
+            imaging(app,i,j); 
+        end
+    else
+        % For even rows, move right to left
+        for j = cols:-1:1
+            calcdisplacement(app, i, j);
+            %pause(waitBeforeMoving(app)) need to figure out how long to
+            %wait 
+
+            %turn the correct laser on 
+            if app.checkBlue == 1
+                turnonLaser(440)
+            elseif app.checkGreen == 1
+                turnonLaser(550)
+            elseif app.checkCyan == 1
+                turnonLaser(470)
+            end
+            %imaging 
+            imaging(app,i,j); 
+        end
+    end
+end
+
+
+
+
+end
