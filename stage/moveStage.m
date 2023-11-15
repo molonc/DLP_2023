77de3f45cf38ac3e98e2f845b98c25fe2adfd019
benrob13 <144270914+benrob13@users.noreply.github.com>
2023-11-15 15:23:12 -0800

    Add files via upload

--- stage/moveStage.m
+++ stage/moveStage.m
@@ -1,5 +1,6 @@
 % moveStage.m
 function moveStage(app,corner)
     command = sprintf('M X=%.2f Y=%.2f Z=%.2f', corner(1), corner(2), corner(3));
-    fprintf(app.stage, command);    
+    fprintf(app.stage, command);
+    waitBeforeMoving(app); %this should wait the correct amount of time 
 end
--- stage/waitBeforeMoving.m
+++ stage/waitBeforeMoving.m
@@ -1,8 +1,11 @@
-function waitBeforeMoving(app, distance)
-    % Calculate the time it takes to move the given distance at maximum speed
-    maxSpeedX = app.stageSpeed;  % Replace with your actual maximum speed for the X-axis
-    timeToMove = abs(distance) / (maxSpeedX * 1000);  % Convert mm to m
-
-    % Wait for the calculated time
-    pause(timeToMove);
+function waitBeforeMoving(app)
+    %waits until the stage is no longer moving 
+    fprintf(app.stage, '/'); % this will retrun the status B meaning busy N meaning no motor runnning
+    response = fscanf(app.stage);
+    pause(abs(app.topleft(1)-app.topright(1))/(3000*23)); % I think this is the wait time for xstep if that makes sense
+    if isequal(response,'N') % hoping that this doesnt mess anything up 
+        return; 
+    else
+        waitBeforeMoving(app);
+    end
 end
