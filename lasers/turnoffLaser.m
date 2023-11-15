3772e78760c5a9d4935c2490e2b7cce0de5f0156
sayeholoumi <53805786+sayeholoumi@users.noreply.github.com>
2023-11-15 12:35:32 -0800

    Add files via upload

---
+++ lasers/turnoffLaser.m
@@ -0,0 +1,16 @@
+function turnoffLaser(app, wavelength)
+    switch wavelength 
+        case 440
+            app.BLUEEditField.Value = 0;
+            app.checkBlue = 0;
+            app.OFFButtonBlue.Text = "OFF";           
+        case 470
+            app.checkGreen = 0;
+            app.OFFButtonGreen.Text = "OFF";           
+            app.GREENEditField.Value = 0;
+        case 550
+            app.checkCyan = 0;
+            app.OFFButtonCyan.Text = "OFF";           
+            app.CyanEditField.Value = 0;
+    end
+end
