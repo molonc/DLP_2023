12067e6943579f11b7898d5f7494c72f878c7224
sayeholoumi <53805786+sayeholoumi@users.noreply.github.com>
2023-11-15 12:37:21 -0800

    Add files via upload

--- lasers/turnoffLaser.m
+++ lasers/turnoffLaser.m
@@ -5,12 +5,12 @@ function turnoffLaser(app, wavelength)
             app.checkBlue = 0;
             app.OFFButtonBlue.Text = "OFF";           
         case 470
-            app.checkGreen = 0;
-            app.OFFButtonGreen.Text = "OFF";           
-            app.GREENEditField.Value = 0;
-        case 550
             app.checkCyan = 0;
             app.OFFButtonCyan.Text = "OFF";           
             app.CyanEditField.Value = 0;
+        case 550
+            app.checkGreen = 0;
+            app.OFFButtonGreen.Text = "OFF";           
+            app.GREENEditField.Value = 0;
     end
 end
--- lasers/turnonLaser.m
+++ lasers/turnonLaser.m
@@ -1,14 +1,14 @@
-function turnonLaser(wavelength)
+function turnonLaser(app, wavelength)
     switch wavelength 
         case 440
             enableBlue(app)
             powerBlue(app, app.BLUEEditField.Value)
         case 470
+            enableCyan(app)
+            powerCyan(app, app.UVEditField.Value)
+        case 550
             enableGreen(app)
             powerGreen(app, app.GREENEditField.Value)
-        case 550
-            enableUV(app)
-            powerUV(app, app.UVEditField.Value)
     end
 end
 
