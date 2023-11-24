2a6d8c53a3ae249199c48f573ccaece71adc36d0
benrob13 <144270914+benrob13@users.noreply.github.com>
2023-11-24 11:20:51 -0800

    Update moveScopeAbsZ.m
    removed nikon conversion

--- stage/moveScopeAbsZ.m
+++ stage/moveScopeAbsZ.m
@@ -1,8 +1,8 @@
 % moveScopeAbsZ.m
 function moveScopeAbsZ (app, position)
     % Convert position from micrometers to microscope units
-    nikonConversion = position / 2.5;
+    %nikonConversion = position / 2.5;
 
     % Move the Z-axis to an absolute position
-    app.ti.ZDrive.MoveAbsolute(nikonConversion);
+    app.ti.ZDrive.MoveAbsolute(position);
 end
