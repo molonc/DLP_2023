da51c4b9c34d9fe293aaa557a069e8b3778186d2
benrob13 <144270914+benrob13@users.noreply.github.com>
2023-11-24 11:23:52 -0800

    Update moveScopeZ.m

--- stage/moveScopeZ.m
+++ stage/moveScopeZ.m
@@ -5,4 +5,8 @@ function moveScopeZ (app, distance)
 
     % Move the Z-axis relative to the current position
     app.ti.ZDrive.MoveRelative(distance);
+    % Wait for the device to complete the move
+    maxTimeouts = 100;  % Set an appropriate value for your application
+    timeoutsResponse = 0;
+    app.ti.ZDrive.WaitForDevice(maxTimeouts, timeoutsResponse);
 end
