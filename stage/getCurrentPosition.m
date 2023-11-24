cb86bfb8e60c6b1f7664e66003e6ab2614f89848
benrob13 <144270914+benrob13@users.noreply.github.com>
2023-11-24 10:52:25 -0800

    Update getCurrentPosition.m
    made it so it also gets the current microscope position

--- stage/getCurrentPosition.m
+++ stage/getCurrentPosition.m
@@ -5,4 +5,17 @@ function getCurrentPosition(app)
     position = sscanf(response, 'A X=%f Y=%f Z=%f');
     app.StagePosition = position;
     %app.CurrentPositionLabel.Text = sprintf('X=%.2f Y=%.2f Z=%.2f', app.StagePosition);
+    %This is for the scope:
+
+    % Define variables to store positions
+    xPosition = 0;
+    yPosition = 0;
+    zPosition = 0;
+    piezoPosition = 0;
+    
+    % Call the GetPosition method
+    app.ti.GetPosition(xPosition, yPosition, zPosition, piezoPosition);
+    
+    app.ScopePosition = zPosition; 
+
 end
