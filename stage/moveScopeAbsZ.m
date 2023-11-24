112ac19b50fc917487a87fd983b26b8f55724e19
benrob13 <144270914+benrob13@users.noreply.github.com>
2023-11-24 11:24:03 -0800

    Update moveScopeAbsZ.m

--- stage/moveScopeAbsZ.m
+++ stage/moveScopeAbsZ.m
@@ -5,4 +5,9 @@ function moveScopeAbsZ (app, position)
 
     % Move the Z-axis to an absolute position
     app.ti.ZDrive.MoveAbsolute(position);
+    
+    % Wait for the device to complete the move
+    maxTimeouts = 100;  % Set an appropriate value for your application
+    timeoutsResponse = 0;
+    app.ti.ZDrive.WaitForDevice(maxTimeouts, timeoutsResponse);
 end
