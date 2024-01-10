b1c8dfb064025d47318f585295e9925275681d8c
benrob13 <144270914+benrob13@users.noreply.github.com>
2024-01-09 16:34:57 -0800

    Delete stage/moveStageZ.m
    there is no Z drive for the stage

--- stage/moveStageZ.m
+++
@@ -1,8 +0,0 @@
-% moveStageZ.m
-function moveStageZ(app, distance)
-    command = sprintf('R Z=%.2f', distance);
-    fprintf(app.stage, command);
-    % Update the position and display it
-    app.StagePosition(3) = app.StagePosition(3) + distance;
-    
-end
