4ab9c0790fa7f02426c28dd936003c55f3062a15
benrob13 <144270914+benrob13@users.noreply.github.com>
2024-01-19 16:39:26 -0800

    Add files via upload

--- testing/scope_testing.m
+++ testing/scope_testing.m
@@ -2,61 +2,32 @@ try
     % Try to create an instance of the Nikon TiScope
     scope = actxserver('Nikon.TiScope.NikonTi');
     
+    get(scope); %not sure this will work but we can try to see the list of propeties 
     % Get the current position of ZDrive
     currentPosition = get(scope.ZDrive.Position, 'DisplayString');
-    laser_serial = serialport('COM3', 9600); 
-    fopen(laser_serial)
-    laser_serial.DataBits = 8;
-    laser_serial.Parity = 'none';
-    laser_serial.StopBits = 1;
-    laser_serial.FlowControl = 'none';
-    configureTerminator(laser_serial, 13);
-
-    % Initialize commands
-    initCmd1 = '57 02 FF 50';
-    initCmd2 = '57 03 AB 50';
-
-    % Write commands to the serial port
-    writeline(laser_serial, initCmd1);
-    writeline(laser_serial, initCmd2);
-
-    % distanceToMove = input('Enter the distance to move (in micrometers): ');
-    % % Move ZDrive 50 micrometers up
-    % distanceToMove = (distanceToMove/2.5)*100;
-    % 
-    % scope.ZDrive.MoveRelative(distanceToMove);
-    % 
-    % % Pause for a moment to allow the ZDrive to move
-    % pause(2);
-    % 
-    % % Get the new position of ZDrive
-    % newPosition = get(scope.ZDrive.Position, 'DisplayString');
-    % 
-    % % Display the new position
-    % disp(['ZDrive moved from ' num2str(currentPosition) ' to ' num2str(newPosition) ' micrometers.']);
-    % laser_serial = serialport('COM3', 9600);
-    % fopen(laser_serial)
-    % laser_serial.DataBits = 8;
-    % laser_serial.Parity = 'none';
-    % laser_serial.StopBits = 1;
-    % laser_serial.FlowControl = 'none';
-    % configureTerminator(laser_serial, 13);
-    % 
-    % % Initialize commands
-    % initCmd1 = '57 02 FF 50';
-    % initCmd2 = '57 03 AB 50';
-    % 
-    % % Write commands to the serial port
-    % writeline(laser_serial, initCmd1);
-    % writeline(laser_serial, initCmd2);
+    disp(currentPosition);
     
-    %get(scope.Lasers.Connect(laser_serial),'DisplayString');
-    % Open the AuxShutter if mounted
-% Check the value of AuxShutter.IsOpened
+    %This is to check the shutter open
+    shutterstatus = get(scope.LU4ALaser.IsOpenShutter,'DisplayString');
+    disp(shutterstatus);
+   
+    scope.LightPathDrive.ChangeLightPath(1) %this is the enum for L100
+    
+    % scope.Lasers.Connect(laser_serial)
+    % Check the value of AuxShutter.IsOpened
     shutterstatus = get(scope.Laser.IsOpenShutter,'DisplayString');
     disp(shutterstatus);
+
+
+    shutterstatus = set(scope.LU4ALaser,'IsOpenShutter', 1); %assuming 1 is open ?!?
+
+    shutterstatus = get(scope.AuxShutter.IsOpened,'DisplayString');
+    disp(shutterstatus);
     
-    scope.Lasers.Connect(laser_serial);
+    shutterstatus = get(scope.EpiShutter.IsOpened,'DisplayString');
+    disp(shutterstatus);
+    %scope.LU4ALaser.SaveObservationModeLU4A();
+    %scope.Lasers.Connect(laser_serial);
 catch exception
     % Display error message
     disp(['Error: ' exception.message]);
